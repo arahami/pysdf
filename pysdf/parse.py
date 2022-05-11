cd6ba35cbee8bfef064a9d8fdb9e8c639bc22e27
arahami <a.rahami88@gmail.com>
2022-05-12 00:06:21 +0430

    [improvement] setup.py

--- pysdf/parse.py
+++ pysdf/parse.py
@@ -8,7 +8,7 @@ from xml.etree.ElementTree import ParseError
 import xml.dom.minidom
 import glob
 
-from tf.transformations import *
+from tf_transformations import *
 
 from pysdf.naming import *
 from pysdf.conversions import *
@@ -24,7 +24,7 @@ if mesh_path_env_name in os.environ:
   catkin_ws_path = os.environ[mesh_path_env_name]
 else:
   catkin_ws_path = os.path.expanduser('~') + '/catkin_ws/src/'
-supported_sdf_versions = [1.4, 1.5, 1.6]
+supported_sdf_versions = [1.4, 1.5, 1.6, 1.7]
 
 catkin_ws_path_exists = os.path.exists(catkin_ws_path)
 
--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-d = generate_distutils_setup(
-  packages=['pysdf'],
-  package_dir={'': 'src'}
+package_name = 'pysdf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # ('share/' + package_name, ['pysdf/conversions.py']),
+        # ('share/' + package_name + "/src", ['pysdf/conversions.py']),
+        # ('share/' + package_name, ['pysdf/naming.py']),
+        # ('share/' + package_name, ['pysdf/parse.py']),
+        ('lib/' + "/python3.8/site-packages/pysdf/scripts" , ['scripts/sdf2urdf.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arahami',
+    maintainer_email='a.rahami88@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "sdf2urdf = pysdf.scripts.sdf2urdf:main",
+        ],
+    },
 )
-
-setup(**d)
